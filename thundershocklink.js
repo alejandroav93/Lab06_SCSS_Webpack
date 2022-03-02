7e4770b2c19a9d69524c8c594aaddc5e68a3e425
alejandroav <alv12429@uvg.edu.gt>
2022-03-02 00:18:17 -0600

    added js/css for thundershock
    added js/css for thundershock

---
+++ thundershocklink.js
@@ -0,0 +1,12 @@
+function change(){
+    document.getElementById("h1_thundershock").innerHTML ="Your Used Thundershock!";
+    document.getElementById("p1_thundershock").innerHTML="Your Pikachu Used Thundershock! It was a critial hit!! Eevee is now Paralized!";
+    document.getElementById("p2_thundershock").innerHTML="What will you do?";
+    document.getElementById("caption_thundershock").innerHTML="Copyright: The Pokémon Company Inc./Game Freak Co., Ltd/ Nintendo Co., Ltd";
+    document.getElementById("summary_thundershock").innerHTML="Battle Moves (dropdown)";
+    document.getElementById("link1").innerHTML="Thundershock";
+    document.getElementById("link2").innerHTML="Tail Whip";
+    document.getElementById("link3").innerHTML="Growl";
+    document.getElementById("label_file").innerHTML="Story progress";
+    document.getElementById("file").value = 50.00;
+}
---
+++ thundershocklink4.js
@@ -0,0 +1,10 @@
+function change(){
+    document.getElementById("h1_thundershock").innerHTML ="Your Used Thundershock!";
+    document.getElementById("p1_thundershock").innerHTML="Your Pikachu Used Thundershock! It reduced Eevee health Points!";
+    document.getElementById("p2_thundershock").innerHTML="Eevee Fainted!";
+    document.getElementById("p3_thundershock").innerHTML="You Defeated Your Rival!";
+    document.getElementById("caption_thundershock").innerHTML="Copyright: The Pokémon Company Inc./Game Freak Co., Ltd/ Nintendo Co., Ltd";
+    document.getElementById("link1").innerHTML="Finish Story";
+    document.getElementById("label_file").innerHTML="Story progress";
+    document.getElementById("file").value = 87.50;
+}
