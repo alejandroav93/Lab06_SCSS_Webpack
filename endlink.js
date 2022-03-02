ef5f509c8448937fb07326e9a3f17f21fcd2f63b
alejandroav <alv12429@uvg.edu.gt>
2022-03-02 00:33:32 -0600

    added end js/css

---
+++ endlink.js
@@ -0,0 +1,9 @@
+function change(){
+    document.getElementById("h1_thundershock").innerHTML ="You have Defeated your Rival!";
+    document.getElementById("p1_thundershock").innerHTML="After a tough battle, you have defeated your rival. You have earned some respect (and cash too!). Let's Prepare for your next battle...";
+    document.getElementById("p2_thundershock").innerHTML="If you want to re-start the story:";
+    document.getElementById("caption_thundershock").innerHTML="Copyright: The Pokémon Company Inc./Game Freak Co., Ltd/ Nintendo Co., Ltd";
+    document.getElementById("link1").innerHTML="Go back to beggining";
+    document.getElementById("label_file").innerHTML="Story progress";
+    document.getElementById("file").value = 100.00;
+}
